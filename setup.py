1ed2c1135165a94db7edddf530629e0e45cbab0f
jonny <>
2019-10-07 19:55:34 +0100

    minimized setup, removed template email etc

--- setup.py
+++ setup.py
@@ -3,18 +3,13 @@ from setuptools import setup
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
     name='core_logic',
-    author='some author',
-    author_email='jladan@uwaterloo.ca',
+    # author='some author',
+    # author_email='',
     # Needed to actually package something
     packages=['core_logic'],
-    # Needed for dependencies
-    #install_requires=['numpy'],
-    # *strongly* suggested for sharing
+    # install_requires=['numpy'], # Not required here
     version='0.1',
-    # The license can be anything you like
     license='MIT',
     description='An example of a python package from pre-existing code',
-    # We will also need a readme eventually (there will be a warning)
-    # long_description=open('README.txt').read(),
 )
 
